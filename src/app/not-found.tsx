7f8f9dec29c11adac1ce98f8c4d2e739d7bcf907
AbdallhGamal19 <abdallhga192000@gmail.com>
2025-06-02 18:55:34 +0300

    vercel error

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,23 +1,25 @@
-// app/not-found.tsx أو pages/404.tsx حسب نوع مشروعك
-
 import Link from "next/link";
-
+import "./[locale]/globals.css";
 export default function NotFound() {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 px-4">
-      <h1 className="text-6xl font-bold text-gray-800 mb-4">404</h1>
-      <h2 className="text-2xl font-semibold text-gray-700 mb-2">
-        Page Not Found
-      </h2>
-      <p className="text-gray-600 mb-6">
-        Sorry, we couldn’t find the page you’re looking for.
-      </p>
-      <Link
-        href="/"
-        className="px-6 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition"
-      >
-        Go Home
-      </Link>
-    </div>
+    <html>
+      <body>
+        <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 px-4">
+          <h1 className="text-6xl font-bold text-gray-800 mb-4">404</h1>
+          <h2 className="text-2xl font-semibold text-gray-700 mb-2">
+            Page Not Found
+          </h2>
+          <p className="text-gray-600 mb-6">
+            Sorry, we couldn’t find the page you’re looking for.
+          </p>
+          <Link
+            href="/"
+            className="px-6 py-2 bg-primary text-white rounded-lg hover:primary-700 transition"
+          >
+            Go Home
+          </Link>
+        </div>
+      </body>
+    </html>
   );
 }
